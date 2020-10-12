8fe84c02de44fe18f7d76e4e66552977deee9fa1
Katerina Kraieva <katika207@gmail.com>
2020-10-12 21:00:53 +0300

    ДЗ 8
    без пролистывание изображений галереи

---
+++ js/gallery-script.js
@@ -0,0 +1,73 @@
+import imagesArr from './gallery-items.js';
+
+const imagesGallery = document.querySelector('.js-gallery');
+const modal = document.querySelector('.js-lightbox');
+const modalImage = document.querySelector('.lightbox__image');
+const modalCloseBtn = document.querySelector('[data-action="close-lightbox"]');
+const modalOverlay = document.querySelector('.lightbox__overlay');
+
+const imagesMarkup = createImageCards(imagesArr);
+
+// создание и рендер разметки
+imagesGallery.insertAdjacentHTML('beforeend', imagesMarkup);
+
+//открытие модалки
+imagesGallery.addEventListener('click', onImagesGalleryClick);
+
+// закрытие модалки через кнопку
+modalCloseBtn.addEventListener('click', modalClose);
+
+// закрытие модалки по Esc
+document.addEventListener('keydown', modalEscClose);
+
+//зактрытие по оверлэю
+modalOverlay.addEventListener('click', onModalOverlayClick);
+
+function createImageCards(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `
+    <li class="gallery__item">
+       <a
+         class="gallery__link"
+         href="${original}"
+       >
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+    </li>`;
+    })
+    .join('');
+}
+function onImagesGalleryClick(evt) {
+  evt.preventDefault();
+
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
+  console.log(evt.target);
+
+  modal.classList.add('is-open');
+
+  modalImage.src = evt.target.dataset.source;
+  modalImage.alt = evt.target.alt;
+}
+function modalClose() {
+  modal.classList.remove('is-open');
+  modalImage.src = '';
+  modalImage.alt = '';
+}
+function modalEscClose(e) {
+  if (e.keyCode === 27) {
+    modalClose();
+  }
+}
+function onModalOverlayClick(evt) {
+  if (evt.currentTarget === evt.target) {
+    modalClose();
+  }
+}
